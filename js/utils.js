c0d0647a9cfd128164b8c16929a64c4df37b6ed2
Belicio Cardoso <belloinfo@gmail.com>
2022-04-08 03:33:52 -0300

    Arquivo de validacao

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+export default function validacao(controlesRef){
+    console.log('teste')
+    for (let controleRef of controlesRef){
+
+        let inputRef = controleRef.children[1]
+
+        inputRef.addEventListener('keyup', () =>{
+
+            if(inputRef.checkValidity()){
+                controleRef.classList.remove('camposErro')
+            } else{
+                controleRef.classList.add('camposErro')
+            }
+        })
+    }
+}
